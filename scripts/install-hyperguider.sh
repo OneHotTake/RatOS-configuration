318085d87d6b365f7e5aa92c0e2aa120c36a8618
OneHotTake <136649517+OneHotTake@users.noreply.github.com>
2023-06-14 16:44:04 -0500

    Add files via upload

--- scripts/install-hyperguider.sh
+++ scripts/install-hyperguider.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n +2 /home/pi/printer_data/config/RatOS/templates/hyperguider.cfg > /home/pi/printer_data/config/printer.cfg
+tail -n +2 /home/pi/printer_data/config/RatOS/templates/hyperguider.printer.template.cfg > /home/pi/printer_data/config/printer.cfg
